962abef5f488b0eb2c80ece2d4bdd4862f7eb218
fc57037 <fc57037@alunos.ciencias.ulisboa.pt>
2023-11-18 20:11:48 +0000

    first commit

---
+++ database.js
@@ -0,0 +1,15 @@
+const menu = [
+    {
+        id: 1,
+        title: 'Süzme Pankek',
+        category: 'breakfast',
+        price: 15.99,
+        img: './images/product1.png',
+        desc: 'Sütün lezzeti buttermilk pankeklerde! Yumuşacık bir dokuya sahip süzme pankeklerimizle gününüze tat katın.',
+    },
+
+];
+
+console.log(menu);
+
+export default menu
